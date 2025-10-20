c57c5aa398166032d9225e29063bda9e9dc6b1c1
HeeSoo <50008659+HeeSoo-Moon96@users.noreply.github.com>
2025-10-21 01:40:26 +0900

    TasnStack Remove 3

--- hooks/mutations/type.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosError } from "axios";
-
-// useMutation의 옵션 타입
-export type UseMutationProps = {
-    onMutate?: () => void;
-    onSuccess?: (data: any, ...args: any[]) => void;
-    onError?: (error: AxiosError) => void;
-    onSettled?: () => void;
-};

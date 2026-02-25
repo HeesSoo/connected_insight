cfb66cccd8584f7ea4751fe0ec474508946dc0da
HeeSoo <halonahaley@naver.com>
2026-02-25 22:45:54 +0900

    Vercel Test Cache

--- app/[lang]/(web)/layout.tsx
+++ app/[lang]/(web)/layout.tsx
@@ -4,6 +4,8 @@ import { navigationConfig } from "@/data/navigation";
 import axios from "axios";
 import { Suspense } from "react";
 
+export const dynamic = 'force-dynamic';
+
 interface WebLayoutProps {
     children: React.ReactNode;
     params: {
